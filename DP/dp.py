1249df713651d653dab47d902e29a09995de8f31
ZBJ_ <905906892@qq.com>
2025-06-10 16:42:47 +0800

    优化DP算法，并调整最大不满足数的计算方式，dp算法基本完结，现todo:通过每个时间的任务到达计算每个时间的任务数

--- DP/dp.py
+++ DP/dp.py
@@ -4,7 +4,7 @@ import numpy as np
 # 读取指定文件夹下的所有txt文件（每个文件都是一段周期所有时刻下的该应用类型任务的任务数），返回一个二维列表  但可能是没有进行降序排列的！
 def read_data_from_txt(folder_path, num_types):
     data = []
-    for i in range(num_types):
+    for i in range(num_types-1, -1, -1):  # 从类型3到类型0
         file_path = os.path.join(folder_path, f"type{i}.txt")
         with open(file_path, "r") as f:
             # 读取所有行，去除空行和换行符，并转为 float
@@ -14,25 +14,25 @@ def read_data_from_txt(folder_path, num_types):
     return data
 
 def main():
-    folder = "data"  # 假设数据文件夹名为data
-    num_types = 3 # 假设有3种类型的需求数据
+    folder = "DP/data"  # 假设数据文件夹名为data
+    num_types = 3       # 文件夹内有多少种应用类型的需求数据
     data = read_data_from_txt(folder, num_types)
     print(data)
-    # # 自拟简化数据：3种类型，每种3个需求值（降序排列）
+    # 自拟简化数据：3种类型，每种3个需求值（降序排列）
     # data = [
     #     [6.0, 5.0, 2.0, 1.0],   # 类型0：需求值降序，如5（最大）、3、1（最小）
     #     [4.0, 2.0, 1.0],   # 类型1
     #     [3.0, 1.0, 1.0]    # 类型2
     # ]
-    # num_types = len(data)                # 3种类型
-    max_unsatisfied_per_type = max(len(data[0]),len(data[1]),len(data[2]))  # 每种最多3个不满足（k=0~3）
-    total_max_unsatisfied = num_types * max_unsatisfied_per_type  # 3*3=9
+    # num_types = len(data)         # 3种类型
+    max_len_per_type = max(len(arr) for arr in data)  # 由于数组长度不统一要对齐数组 取所有应用类型中的最大长度（k=0~3）
+    total_max_unsatisfied = num_types * max_len_per_type  # 3*3=9  取最大长度* 类型数就是最大不满足数
 
     # 扩展数据到二维数组a，行数为num_types，列数为total_max_unsatisfied+1 与data内容一致，但保证了数组长度平齐
     # 不是data前缀和的原因是因为预测得到的就是当前时刻的任务“处理”数，已经包含了任务处理时长的因素，如果是任务到达数那就需要累加了
     a = [[float('0')] * (total_max_unsatisfied + 1) for _ in range(num_types)]
     for i in range(num_types):
-        for j in range(max_unsatisfied_per_type + 1):
+        for j in range(max_len_per_type + 1):
             if j < len(data[i]):
                 a[i][j] = data[i][j]
 
@@ -70,16 +70,17 @@ def main():
             print(f"j={j}: {dp[i][j]}", end=" | ")
         print("\n" + "-" * 50)
 
-    t = 3  # 假设一开始不满足3个需求
+    t = 2  # 假设一开始不满足3个需求
     now_sla = t
     for i in range(num_types-1,-1,-1):
-        print(f"应用类型{i}: {now_sla-path[i][t]}", end=" | ")
+        print(f"应用类型{i}允许违规数: {now_sla-path[i][t]}, 执行任务数为{a[i][now_sla-path[i][t]]}", end="\n")
         now_sla = path[i][t]
         t = path[i][t]
         
         
 if __name__ == "__main__":
     main()
+
 #  这个dp过程发现一个问题，在可能允许较多违规的情况下，它可能会把某种应用类型的任务的高需求值通通滤过去，
 #  使得总需求值过低，导致可能某个应用类型的任务都不想去满足，使得其虚拟机很少，是不是应该设置个阈值
 
